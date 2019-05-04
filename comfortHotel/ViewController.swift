c969b889765d365328386e0a74abde37716330dd
akapan <akapan.r@hitachi-tstv.com>
2019-05-04 15:04:37 +0700

    Work Shop Comfort Hotel

---
+++ comfortHotel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PuService_04052019
+//
+//  Created by TST-APP-02 on 4/5/2562 BE.
+//  Copyright © 2562 Hitachi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

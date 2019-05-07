d89f868a97b7a4f20fe26654b99a7e23784ed73a
akapan <akapan.r@hitachi-tstv.com>
2019-05-07 15:16:31 +0700

    optional edit for get value from text field

--- RegisterViewController.swift
+++ RegisterViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class RegisterViewController: UIViewController {
     
+    var name:String?
+    var user:String?
+    var pass:String?
+
     
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var usernameTextField: UITextField!
@@ -24,7 +28,15 @@ class RegisterViewController: UIViewController {
     
     @IBAction func uploadButton(_ sender: UIBarButtonItem) {
         
+//    Get value from textfield
+        name = nameTextField.text
+        user = usernameTextField.text
+        pass = passwordTextField.text
         
+//    Show log
+        print("name ==> \(String(describing: name))")
+        print("user ==> \(String(describing: user))")
+        print("pass ==> \(String(describing: pass))")
         
     }
     

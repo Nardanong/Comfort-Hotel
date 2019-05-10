ed046c38e460fe66bfe6b7f47615f6ff91a042b9
akapan <akapan.r@hitachi-tstv.com>
2019-05-10 11:48:06 +0700

    Check user in database  (Have and No have) success

--- comfortHotel/SigninController.swift
+++ comfortHotel/SigninController.swift
@@ -22,6 +22,46 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }//Main Method
     
+//  Check Authen
+    func checkAuthen(user: String, password: String) -> Void {
+        let myConstant = Myconstant()
+        let urlPHP = myConstant.fineJSONWhereUser(user: user)
+        print("urlPHP ==> \(urlPHP)")
+        
+        guard let url = URL(string: urlPHP) else {
+            return
+        }//Guard
+        
+//      task
+        let task = URLSession.shared.dataTask(with: url) {(data, response, error) in
+            
+            guard let dataResponse = data, error == nil else{
+               print("Have Error")
+                return
+            }//Guard
+            
+//          do try catch
+//          do = คือสิ่งที่ต้องทำ ส่งค่าออกมาเป็น - JSON
+            do {
+                let jsonResponse = try JSONSerialization.jsonObject(with: dataResponse, options: [])
+                print("JSONResponse ==> \(jsonResponse)")
+            }catch let myError {
+                print(myError)
+//              ดูค่า  user  ใน database
+                print("No have user \(user) in database")
+                
+//              ขัดจังหวะการทำงานให้เลิก ทำสิ่งที่กำลังทำอยู่ แล้วให้มาทำรายการที่ 2 ก่อน ถ้าไม่ใช้ DispatchQueue จะเกิด  Error
+//              Check username in database
+                DispatchQueue.main.async {
+//                   Call use function myAlert
+                    self.myAlert(title: "User False", message: "No have user \(user) in database")
+                }
+            }//Do
+            
+        } //End task
+        task.resume()
+    }//checkAuthen function
+    
 //  function Show Alert
     func myAlert(title: String, message:String) -> Void {
         let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertController.Style.alert)
@@ -45,6 +85,7 @@ class ViewController: UIViewController {
         
     }//checkSpace
 
+// Button Sign in
     @IBAction func SinginButton(_ sender: UIButton) {
 //      Get Value from TextField
         username = userTextField.text
@@ -57,6 +98,8 @@ class ViewController: UIViewController {
             myAlert(title: "Have Space", message: "Please fill all blank")
         }else{
             print("No Space")
+//          Call function CheckAuthen for check login
+            checkAuthen(user: username!, password: password!)
         }
         
 //      Show Log
--- myconstant.swift
+++ myconstant.swift
@@ -11,10 +11,17 @@ import Foundation
 //Web Service
 class Myconstant {
     
+//  web service recive value from database
+    func fineJSONWhereUser(user: String) -> String {
+        let result = "http://www.hitachi-tstv.com/test_ios/getUserWhereUserPu.php?isAdd=true&username=\(user)"
+        return result
+        
+    }//fineJSONWhereUser
+    
+//  Web service send value add user to database
     func findURLAddUser(name:String,username:String,password:String) -> String {
         let result = "http://www.hitachi-tstv.com/test_ios/addUserPu.php?isAdd=true&name=\(name)&username=\(username)&password=\(password)"
         return result
     }
-    
     
 } //myconstant class

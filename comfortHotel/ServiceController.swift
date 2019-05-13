e8bfa6a58d4a4f4a0a73085203b067d8a58a790e
akapan <akapan.r@hitachi-tstv.com>
2019-05-13 10:57:33 +0700

    insert info Map success

--- comfortHotel/ServiceController.swift
+++ comfortHotel/ServiceController.swift
@@ -7,24 +7,68 @@
 //
 
 import UIKit
+import MapKit
+
 
 class ServiceController: UIViewController {
+    
+    @IBOutlet weak var nyMapView: MKMapView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+//      สิ่งที่จะเกิดขึ้นครั้งแรก
+//      showMap(last: 13.571689, lang: 100.765554)
+        showMapType2(last: 13.571689, lang: 100.765554)
+        
+    } //Main Method
+    
+    func showMapType2(last: Double, lang: Double) -> Void {
+        let latitude: CLLocationDegrees = last
+        let longtitude: CLLocationDegrees = lang
+        
+//      Zoom Value
+        let laDelta: CLLocationDegrees = 0.01
+        let longDelta: CLLocationDegrees = 0.01
+        let span: MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: laDelta, longitudeDelta: longDelta)
+//      พิกัด
+        let addressCompany: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: latitude, longitude: longtitude)
+        let region:MKCoordinateRegion = MKCoordinateRegion(center: addressCompany, span: span)
+        
+        self.nyMapView.setRegion(region, animated: true)
+        
+//      Company Anotation (Info)
+        let companyAnotation = MKPointAnnotation()
+        companyAnotation.coordinate = addressCompany
+        companyAnotation.title = "Address Company"
+        companyAnotation.subtitle = "Hitachi Transport Vantec"
+        self.nyMapView.addAnnotation(companyAnotation)
+        
+//      Bus Stop
+        let latitudeBus: CLLocationDegrees = 13.571273
+        let longtitudeBus: CLLocationDegrees = 100.763647
+        
+        let busStopAnotation = MKPointAnnotation()
+        busStopAnotation.coordinate = CLLocationCoordinate2D(latitude: latitudeBus, longitude: longtitudeBus)
+        busStopAnotation.title = "Bus Stop"
+        busStopAnotation.subtitle = "Bus Stop park 234,456,788"
+        self.nyMapView.addAnnotation(busStopAnotation)
+        
     }
     
+    func showMap(last: Double, lang: Double) -> Void {
+        let homeCordinate = CLLocationCoordinate2DMake(last, lang)
+//      ระยะซูม
+        let regionDistance: CLLocationDistance = 1000
+        let regionSpan = MKCoordinateRegion.init(center: homeCordinate, latitudinalMeters: regionDistance, longitudinalMeters: regionDistance)
+        let options = [MKLaunchOptionsMapCenterKey: NSValue(mkCoordinate: regionSpan.center),MKLaunchOptionsMapSpanKey: NSValue(mkCoordinateSpan: regionSpan.span)]
+//      ตัวปักหมุด
+        let placemark = MKPlacemark(coordinate: homeCordinate)
+//      จุดที่จะปักหมุด
+        let mapItem = MKMapItem(placemark: placemark)
+        mapItem.name = "Address Company"
+        mapItem.openInMaps(launchOptions: options)
 
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
     }
-    */
-
-}
+}//Main Class
